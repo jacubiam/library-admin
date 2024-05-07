f474cbedf55d212c7dc46e6980d255eb7add8557
Joaquin Juarez <joaquinjuarez17@hotmail.com>
2024-05-06 20:19:22 -0600

    :sparkles:feat(api):add validation to data and return its respective response code

--- api/books.php
+++ api/books.php
@@ -2,80 +2,133 @@
 spl_autoload_register(function ($class_name) {
     include "../services/" . strtolower($class_name) . '.services.php';
 });
+include_once "../services/utils.php";
 
 $url = "../db/books.csv";
 $urlReserv = "../db/reservations.csv";
 
 if ($_SERVER['REQUEST_METHOD'] === "GET") {
+    header('Content-type: application/json');
     if (isset($_GET['query'])) {
         if ($_GET['query'] === "get_all") {
             $books = Book::get_all($url);
             sort($books);
-            header('Content-type: application/json');
             echo json_encode($books);
+            exit();
         }
 
         if ($_GET['query'] === "get_book") {
             $get_book = Book::get_item($_GET['id'], $url);
             if (gettype($get_book) === "array") {
-                http_response_code(404);    
+                http_response_code(404);
             }
-            header('Content-type: application/json');
             echo json_encode($get_book);
+            exit();
         }
 
-        if (isset($_GET['search'])) {
-            $books = Book::get_all($url);
-            $search = $_GET['search'];
-            $books_filtered = array_filter($books, function ($book) use ($search) {
-                return (strtolower($book[$_GET['query']]) === strtolower($search));
-            });
-            $books_filtered = array_slice($books_filtered, 0);
-            sort($books_filtered);
-            if (count($books_filtered) !== 0) {
-                header('Content-type: application/json');
-                echo json_encode($books_filtered);
+        if (count($_GET) > 1) {
+            if (isset($_GET['search'])) {
+                $books = Book::get_all($url);
+                $search = $_GET['search'];
+
+                $keys = ["title", "author", "genre", "year"];
+                if (array_search(strtolower($_GET['query']), $keys) === false) {
+                    http_response_code(404);
+                    echo json_encode(array("res" => "Query not found"));
+                    exit();
+                }
+
+                $books_filtered = array_filter($books, function ($book) use ($search) {
+                    return (strtolower($book[$_GET['query']]) === strtolower($search));
+                });
+                $books_filtered = array_slice($books_filtered, 0);
+                sort($books_filtered);
+
+                if (count($books_filtered) !== 0) {
+                    echo json_encode($books_filtered);
+                    exit();
+                } else {
+                    http_response_code(404);
+                    echo json_encode(array("res" => "$search not found"));
+                    exit();
+                }
             } else {
-                header('Content-type: application/json');
-                http_response_code(404);
-                echo json_encode(array("not_found" => "$search not found"));
+                http_response_code(400);
+                echo json_encode(array("res" => "Unknown extra parameters"));
+                exit();
             }
         }
+
+        http_response_code(404);
+        echo json_encode(array("res" => "Query not found"));
+        exit();
+    } else {
+        http_response_code(400);
+        echo json_encode(array("res" => "GET request needs a query parameter"));
+        exit();
     }
 }
 
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    header('Content-type: application/json');
     $raw_data = file_get_contents('php://input');
     $post_data = json_decode($raw_data, true);
+
+    //If a json is not passed:
+    if ($post_data === null) {
+        http_response_code(400);
+        echo json_encode(array("res" => "JSON Expected"));
+        exit();
+    }
+
+    $verifier = validator($post_data, "create");
+    if (!$verifier['status']) {
+        http_response_code(400);
+        echo json_encode(array("res" => $verifier['message']));
+        exit();
+    }
 
     $new_book = new Book($post_data);
     $new_book->create_item();
 
-    header('Content-type: application/json');
-    echo json_encode($post_data);
+    echo json_encode($new_book);
 }
 
 if ($_SERVER['REQUEST_METHOD'] === "PUT") {
+    header('Content-type: application/json');
     $raw_data = file_get_contents('php://input');
     $put_data = json_decode($raw_data, true);
 
+    //If a json is not passed:
+    if ($put_data === null) {
+        http_response_code(400);
+        echo json_encode(array("res" => "JSON Expected"));
+        exit();
+    }
+
+    $verifier = validator($put_data, "edit");
+    if (!$verifier['status']) {
+        http_response_code(400);
+        echo json_encode(array("res" => $verifier['message']));
+        exit();
+    }
+
     $get_book = Book::get_item($put_data['id'], $url);
-    header('Content-type: application/json');
     if (gettype($get_book) === "array") {
         http_response_code(404);
         echo json_encode($get_book);
         exit();
     }
 
-    $get_reserv = Reservation::get_item($put_data['id'], $urlReserv);  
+    $get_reserv = Reservation::get_item($put_data['id'], $urlReserv);
     if (!(gettype($get_reserv) === "array")) {
-        http_response_code(404);
+        http_response_code(403);
         echo json_encode(array("res" => "A Borrowed book cannot be edited"));
         exit();
     }
-    
+
     $get_book->edit_item($put_data);
-    echo json_encode($put_data);
+    echo json_encode($get_book);
 }
 
 if ($_SERVER['REQUEST_METHOD'] === "DELETE") {
@@ -85,6 +138,21 @@ if ($_SERVER['REQUEST_METHOD'] === "DELETE") {
         $id = end($uri);
 
         $del_book = Book::get_item($id, $url);
+        if (gettype($del_book) === "array") {
+            header('Content-type: application/json');
+            http_response_code(404);
+            echo json_encode($del_book);
+            exit();
+        }
+
+        $get_reserv = Reservation::get_item($id, $urlReserv);
+        if (!(gettype($get_reserv) === "array")) {
+            header('Content-type: application/json');
+            http_response_code(403);
+            echo json_encode(array("res" => "A Borrowed book cannot be deleted"));
+            exit();
+        }
+
         $del_book->delete_item();
         http_response_code(204);
     } else {
---
+++ services/utils.php
@@ -0,0 +1,124 @@
+<?php
+function validator($data, $context)
+{
+    $result = array("status" => true, "message" => "none");
+    $model_keys = [];
+
+    foreach ($data as $value) {
+        if (empty($value)) {
+            $result['status'] = false;
+            $result['message'] = "Empty field found";
+            return $result;
+        }
+    }
+
+    if ($context === "create") {
+        if (count($data) !== 6) {
+            $result['status'] = false;
+            $result['message'] = "Unexpected amount of fields, sent " . count($data) . ", expected (6).";
+            return $result;
+        }
+
+        //Match excpected keys to submitted data
+        $model_keys = ["title", "author", "pages", "genre", "year", "status"];
+    }
+
+    if ($context === "edit") {
+        if (count($data) !== 7) {
+            $result['status'] = false;
+            $result['message'] = "Unexpected amount of fields, sent " . count($data) . ", expected (7).";
+            return $result;
+        }
+
+        //Match excpected keys to submitted data
+        $model_keys = ["id", "title", "author", "pages", "genre", "year", "status"];
+    }
+    
+    if ($context === "loan") {
+        if (count($data) !== 2) {
+            $result['status'] = false;
+            $result['message'] = "Unexpected amount of fields, sent " . count($data) . ", expected (7).";
+            return $result;
+        }
+
+        //Match excpected keys to submitted data
+        $model_keys = ["id", "user_name"];
+    }
+
+    $data_keys = array_keys($data);
+    $diff_keys = array_diff($data_keys, $model_keys);
+
+    if (count($diff_keys) !== 0) {
+        $result['status'] = false;
+        $result['message'] = "Unexpected field: " . reset($diff_keys);
+        return $result;
+    }
+
+    foreach ($data as $key => $value) {
+        if ($key === "id") {
+            if (!preg_match("/^(\d)+$/", $value)) {
+                $result['status'] = false;
+                $result['message'] = "$key only allows integers";
+                return $result;
+            }
+        }
+
+        if ($key === "title") {
+            if (!preg_match("/^(\w'?\.?\s?)+$/", $value)) {
+                $result['status'] = false;
+                $result['message'] = "$key only allows alphanumeric chars";
+                return $result;
+            }
+        }
+
+        if ($key === "author") {
+            if (!preg_match("/^([a-zA-Z]'?\.?\s?)+$/", $value)) {
+                $result['status'] = false;
+                $result['message'] = "$key only allows alphabetical chars";
+                return $result;
+            }
+        }
+
+        if ($key === "pages") {
+            if (!preg_match("/^\d+$/", $value) || $value <= 0) {
+                $result['status'] = false;
+                $result['message'] = "$key only allows positive integers";
+                return $result;
+            }
+        }
+
+        if ($key === "genre") {
+            if (!preg_match("/^([a-zA-Z]\s?)+$/", $value)) {
+                $result['status'] = false;
+                $result['message'] = "$key only allows alphabetical chars";
+                return $result;
+            }
+        }
+
+        if ($key === "year") {
+            if (!preg_match("/^\-?\d+$/", $value)) {
+                $result['status'] = false;
+                $result['message'] = "$key only allows numeric chars";
+                return $result;
+            }
+        }
+
+        if ($key === "status") {
+            if (!preg_match("/^(available|unavailable)$/", $value)) {
+                $result['status'] = false;
+                $result['message'] = "$key only allows 'available' or 'unavailable'";
+                return $result;
+            }
+        }
+
+        if ($key === "user_name") {
+            if (!preg_match("/^([a-zA-Z]'?\.?\s?)+$/", $value)) {
+                $result['status'] = false;
+                $result['message'] = "$key only allows alphabetical chars";
+                return $result;
+            }
+        }
+    }
+
+    return $result;
+}
